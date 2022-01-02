c8f86221fc5044068c1bfd69bb136c5e734f97b7
Alex Wilson <gpg@probablyfine.co.uk>
2022-01-02 16:27:42 +0000

    feat(main): Button to copy description to clipboard

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ import(runtime.getURL('describe.js'))
   .then((module) => {
     let guesses = module.getTileStates();
 
-    console.log(guesses.map((guess, index) => `${index+1}. ${module.describeGuess(guess)}`));
+    let description = guesses.map((guess, index) => `${index+1}. ${module.describeGuess(guess)}`).join("\n");
+
+    let copyToClipboard = document.createElement('button');
+    copyToClipboard.textContent = 'Copy description to clipboard';
+
+    copyToClipboard.addEventListener('click', (e) => navigator.clipboard.writeText(description), false);
+
+    document.body.prepend(copyToClipboard);
   });
